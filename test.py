e15ebdec9995ed8d1604fc3b11d4f13b14a8c67c
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2017-11-11 17:16:03 +0100

    Inizio macchina a stati

--- test.py
+++ test.py
@@ -4,31 +4,72 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 import logging
 
+class QuestionSearch:
+    
+    def __init__(self):
+        pass
+    def lookup(self, title):
+        
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-def messages_handler(bot, update):
+class Conversation:
+    states = {}
+    STARTED = 0
+    CHIEDI = 1
+    RISPONDI = 2
     
-    if update.message.text == "ciao":
+    
+    def messages_handler(bot, update):
+        if self.states[update.message.chat.id] == Null:
+            self.states[update.message.chat.id] = STARTED
+        elif self.states[update.message.chat.id] == STARTED
+            if update.message.text == "CHIEDI":
+                self.states[update.message.chat.id] = CHIEDI
+                reply_markup = ReplyKeyboardRemove(selective=False)
+                update.message.reply_text("Inserisci la tua domanda:", reply_markup=reply_markup)
+            if update.message.text == "RISPONDI":
+                self.states[update.message.chat.id] = RISPONDI
+                reply_markup = ReplyKeyboardRemove(selective=False)
+                update.message.reply_text("Ecco il quesito:", reply_markup=reply_markup)
+                self.rispondi()
+            else:
+                update.message.reply_text("Premi /help per aiuto")
+        elif self.states[update.message.chat.id] == CHIEDI:
+            self.chiedi()
+        elif self.states[update.message.chat.id] == RISPONDI:
+            self.rispondi()
+
+    def chiedi(self):
+        pass
+        
+
+    def start(bot, update):
         update.message.reply_text("Salve " + update.message.from_user.first_name)
-    else:
-        update.message.reply_text("Premi /help per aiuto")
+        
+        keyboard = [[KeyboardButton("CHIEDI"),
+                     KeyboardButton("RISPONDI")]]
 
-def problems(bot, update):
+        reply_markup = ReplyKeyboardMarkup(keyboard)
+        update.message.reply_text('Cosa vuoi fare?', reply_markup=reply_markup)
+        states[update.message.chat.id] = STARTED
+        
+    def problems(bot, update):
     
-    keyboard = [[KeyboardButton("CHIEDI"),
-                 KeyboardButton("RISPONDI")]]
+        keyboard = [[KeyboardButton("CHIEDI"),
+                     KeyboardButton("RISPONDI")]]
 
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+        reply_markup = ReplyKeyboardMarkup(keyboard)
+        update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
     
 
-def help(bot, update):
-    update.message.reply_text("/problems -> Chiedi domanda - Fai domanda")
+    def help(bot, update):
+        update.message.reply_text("/problems -> Chiedi domanda - Fai domanda")
 
 def main():
     
@@ -40,6 +81,7 @@ def main():
     # Gestione messaegi ricevuti
     dp.add_handler(MessageHandler(Filters.text, messages_handler))
     dp.add_handler(CommandHandler('problems', problems))
+    dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
 
     updater.start_polling()  # Inzio del polling
